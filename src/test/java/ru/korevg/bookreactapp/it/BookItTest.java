874b9be07c6a08fda9ab6cb5e90e12ec2d5d77c8
koreginev <evgkoregin@gmail.com>
2025-03-31 15:45:35 +0300

    Add integration book test

--- src/test/java/ru/korevg/bookreactapp/it/BookItTest.java
+++ src/test/java/ru/korevg/bookreactapp/it/BookItTest.java
@@ -1,15 +1,22 @@
 package ru.korevg.bookreactapp.it;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import ru.korevg.bookreactapp.dto.BookDTO;
 
+import java.math.BigDecimal;
+
+@DirtiesContext
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookItTest extends ITBase {
 
     private static final String TEST_BOOK_ISBN = "9781503280786";
+    private static final String NEW_BOOK_ISBN = "978-5-4461-1107-7";
 
     @LocalServerPort
     private int port;
@@ -24,6 +31,7 @@ public class BookItTest extends ITBase {
     }
 
     @Test
+    @Order(1)
     public void findBook() {
         String json = "{\n" +
                 "  \"id\": 10,\n" +
@@ -38,5 +46,68 @@ public class BookItTest extends ITBase {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody().json(json);
+    }
+
+    @Test
+    @Order(2)
+    public void createBook() {
+        BookDTO newBook = new BookDTO();
+        newBook.setTitle("Философия Java");
+        newBook.setAuthor("Эккель Брюс");
+        newBook.setIsbn(NEW_BOOK_ISBN);
+        newBook.setPrice(BigDecimal.valueOf(3130));
+
+        String json = "{\n" +
+                "  \"id\": 11,\n" +
+                "  \"title\": \"Философия Java\",\n" +
+                "  \"author\": \"Эккель Брюс\",\n" +
+                "  \"isbn\": \"" + NEW_BOOK_ISBN + "\",\n" +
+                "  \"price\": 3130.0,\n" +
+                "  \"content\": null\n" +
+                "}";
+
+        webClient.post()
+                .uri("/books")
+                .bodyValue(newBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().json(json);
+    }
+
+    @Test
+    @Order(3)
+    public void updateBook() {
+        BookDTO newBook = new BookDTO();
+        newBook.setTitle("Moby Dick");
+        newBook.setAuthor("Herman Melville");
+        newBook.setIsbn(TEST_BOOK_ISBN);
+        newBook.setPrice(BigDecimal.valueOf(20.99));
+
+        String json = "{\n" +
+                "  \"id\": 10,\n" +
+                "  \"title\": \"Moby Dick\",\n" +
+                "  \"author\": \"Herman Melville\",\n" +
+                "  \"isbn\": \"9781503280786\",\n" +
+                "  \"price\": 20.99,\n" +
+                "  \"content\": null\n" +
+                "}";
+
+        webClient.put()
+                .uri("/books/isbn/" + TEST_BOOK_ISBN)
+                .bodyValue(newBook)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().json(json);
+    }
+
+    @Test
+    @Order(4)
+    public void deleteBook() {
+        webClient.delete().uri("/books/isbn/" + TEST_BOOK_ISBN)
+                .exchange()
+                .expectStatus().isOk();
+        webClient.get().uri("/books/isbn/" + TEST_BOOK_ISBN)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 }
--- src/test/java/ru/korevg/bookreactapp/it/ITBase.java
+++ src/test/java/ru/korevg/bookreactapp/it/ITBase.java
@@ -1,12 +1,12 @@
 package ru.korevg.bookreactapp.it;
 
-
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -23,6 +23,9 @@ public class ITBase {
             .withEnv("RABBITMQ_DEFAULT_USER", "book")
             .withEnv("RABBITMQ_DEFAULT_PASS", "password");
 
+    @Container
+    static final ElasticsearchContainer ELASTICSEARCH_CONTAINER = new ElasticsearchContainer("elasticsearch:7.17.0");
+
 
     @DynamicPropertySource
     static void postgresProperties(DynamicPropertyRegistry registry) {
@@ -37,11 +40,20 @@ public class ITBase {
                 + "/" + POSTGRES_CONTAINER.getDatabaseName());
         registry.add("spring.flyway.user", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.flyway.password", POSTGRES_CONTAINER::getPassword);
+        POSTGRES_CONTAINER.waitingFor(Wait.forHealthcheck());
     }
 
     @DynamicPropertySource
     static void rabbitProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.rabbitmq.host", RABBIT_MQ_CONTAINER::getHost);
         registry.add("spring.rabbitmq.port", RABBIT_MQ_CONTAINER::getFirstMappedPort);
+    }
+
+    @DynamicPropertySource
+    static void elasticsearchProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.elasticsearch.rest.uris", () -> "http://" +
+                ELASTICSEARCH_CONTAINER.getHost() + ":" + ELASTICSEARCH_CONTAINER.getFirstMappedPort()
+        );
+        ELASTICSEARCH_CONTAINER.waitingFor(Wait.forHttp("/").forStatusCode(200));
     }
 }
